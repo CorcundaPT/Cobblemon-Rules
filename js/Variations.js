4bb28da34ce6796dd08e681245b436a665a3e86a
CorcundaPT <79412616+CorcundaPT@users.noreply.github.com>
2025-02-04 23:50:21 +0000

    Add files via upload

---
+++ js/Variations.js
@@ -0,0 +1,34 @@
+//lista de variações para season
+var SingleRNGMono = "26,56%";
+var SingleSameMono = "13,28%";
+var SingleUserMono = "0,13%";
+var DoubleRNGMono = "39,84%";
+var DoubleSameMono = "19,92%";
+var DoubleUserMono = "0,27%";
+//Exportar seasons para HTML
+document.getElementById("SingleRNGMono").innerHTML = SingleRNGMono;
+document.getElementById("SingleSameMono").innerHTML = SingleSameMono;
+document.getElementById("SingleUserMono").innerHTML = SingleUserMono;
+document.getElementById("DoubleRNGMono").innerHTML = DoubleRNGMono;
+document.getElementById("DoubleSameMono").innerHTML = DoubleSameMono;
+document.getElementById("DoubleUserMono").innerHTML = DoubleUserMono;
+//lista de variações para Eventos
+var SingleRNGTeam = "25,46%";
+var SingleSameTeam = "12,74%";
+var SingleUserTeam = "0,13%";
+var DoubleRNGTeam = "38,22%";
+var DoubleSameTeam = "19,11%";
+var DoubleUserTeam = "0,51%";
+var TripleRNGTeam = "1,27%";
+var TripleSameTeam = "2,55%";
+var TripleUserTeam = "0%";
+//Exportar Eventos para HTML
+document.getElementById("SingleRNGTeam").innerHTML = SingleRNGTeam;
+document.getElementById("SingleSameTeam").innerHTML = SingleSameTeam;
+document.getElementById("SingleUserTeam").innerHTML = SingleUserTeam;
+document.getElementById("DoubleRNGTeam").innerHTML = DoubleRNGTeam;
+document.getElementById("DoubleSameTeam").innerHTML = DoubleSameTeam;
+document.getElementById("DoubleUserTeam").innerHTML = DoubleUserTeam;
+document.getElementById("TripleRNGTeam").innerHTML = TripleRNGTeam;
+document.getElementById("TripleSameTeam").innerHTML = TripleSameTeam;
+document.getElementById("TripleUserTeam").innerHTML = TripleUserTeam;
